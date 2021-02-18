26de32a3da3a0bd5df1ea0d0dc0c2f2a567dc229
Craig Campbell <emtcampbell@outlook.com>
2021-02-18 15:56:43 -0600

    comment out mongoose setup

--- src/app.ts
+++ src/app.ts
@@ -27,35 +27,41 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 3001;
 
-mongoose.connection
-  .on('connected', function () {
-    debug(chalk.blue('Connected', process.env.MONGODB_URI));
-  })
-  .on('error', function (err) {
-    debug(chalk.red('Error', process.env.MONGODB_URI));
-    if (err) {
-      debug(chalk.red(err));
-    }
-  })
-  .on('disconnected', function () {
-    debug(chalk.blue('\nDisconnected', process.env.MONGODB_URI));
-  })
-  .on('open', function () {
-    debug(chalk.blue('Opened', process.env.MONGODB_URI));
+debug(`Port: ${port}`);
 
-    app.listen(port, () => {
-      debug(`listening on port ${chalk.green(port)}`);
-    });
-  });
+app.listen(port, function () {
+  debug('Listening on port ' + port);
+});
 
-if (process.env.MONGODB_URI) {
-  mongoose.connect(process.env.MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  });
-} else {
-  debug(chalk.red('Error mongodb://.env'));
-  debug(chalk.red('Error http://.env'));
-}
+// mongoose.connection
+//   .on('connected', function () {
+//     debug(chalk.blue('Connected', process.env.MONGODB_URI));
+//   })
+//   .on('error', function (err) {
+//     debug(chalk.red('Error', process.env.MONGODB_URI));
+//     if (err) {
+//       debug(chalk.red(err));
+//     }
+//   })
+//   .on('disconnected', function () {
+//     debug(chalk.blue('\nDisconnected', process.env.MONGODB_URI));
+//   })
+//   .on('open', function () {
+//     debug(chalk.blue('Opened', process.env.MONGODB_URI));
+
+//     app.listen(port, () => {
+//       debug(`listening on port ${chalk.green(port)}`);
+//     });
+//   });
+
+// if (process.env.MONGODB_URI) {
+//   mongoose.connect(process.env.MONGODB_URI, {
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//   });
+// } else {
+//   debug(chalk.red('Error mongodb://.env'));
+//   debug(chalk.red('Error http://.env'));
+// }
